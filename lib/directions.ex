3b71c6d33d247b35b28c067793cff6b22950af33
Cássio Marques <cassio.marques@multiverse.io>
2022-11-01 15:26:17 +0000

    Prepare for publishing 0.0.1 to Hex

--- lib/directions.ex
+++ lib/directions.ex
@@ -1,4 +1,27 @@
 defmodule Directions do
+  @moduledoc """
+  Directions is a small Elixir library aimed at generating URLs that point to a Phoenix application.
+
+  The primary use case is when there are different Phoenix web applications that need to display HTML links pointing to each other.
+
+  Imagine for example two applications, A and B. If we wanted to display an HTML link in A that points to a route in B, without Directions we would need to hardcode that URL path, including any path params. Directions ensures that only valid URLs can be generated, which is done by configuring application A to read a file containing the output of running `mix phx.routes` in application B.
+
+  ## Example
+
+  In the target application, run `mix phx.routes [RouterModule] > /path/to/output.txt`. Then configure your application to load that file:
+
+        # Multiple different route files can be provided in the config below
+        config :directions,
+          route_sources: [
+            {:shop, "http://shop.com", Path.expand("../shared_routes/shop_routes.txt", __DIR__)}
+          ]
+
+  The above will read that file and keep it in the library's state. Now, whenever your application needs to generate a link pointing to the external Phoenix web app, use something like:
+
+        # This will generate a URL like http://shop.com/products/123
+        <%= link "A Great Product!", to: Directions.url(:shop, :product_path, :show, id: 123)
+  """
+
   alias Directions.{Resolver, RoutesDB, SearchTerm}
 
   @doc """
@@ -13,16 +36,15 @@ defmodule Directions do
   - `path_params`: Keyword list with the path params to be interpolated in the generated URL.
 
   ## Examples
-  ```
-  Directions.url(:shop, :product_path, :new)
-  # => "http://shop.com/products/new"
 
-  Directions.url(:shop, :product_path, :show, id: 123)
-  # => "http://shop.com/products/123"
+        Directions.url(:shop, :product_path, :new)
+        # => "http://shop.com/products/new"
 
-  Directions.url(:shop, :product_path, :edit, id: 123)
-  # => "http:shop.com/products/123/edit"
-  ```
+        Directions.url(:shop, :product_path, :show, id: 123)
+        # => "http://shop.com/products/123"
+
+        Directions.url(:shop, :product_path, :edit, id: 123)
+        # => "http:shop.com/products/123/edit"
   """
   @spec url(group_name :: atom(), route_name :: atom(), action :: atom(), path_params :: keyword()) :: String.t()
   def url(group_name, route_name, action, path_params \\ []) do
--- mix.exs
+++ mix.exs
@@ -1,17 +1,29 @@
 defmodule Directions.MixProject do
   use Mix.Project
 
+  @scm_url "https://github.com/Multiverse-io/directions"
+  @version "0.0.1"
+
   def project do
     [
       app: :directions,
-      version: "0.1.0",
-      elixir: "~> 1.14",
+      version: @version,
+      elixir: "~> 1.11",
+      elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      package: package(),
+      deps: deps(),
+      source_url: @scm_url,
+      description: """
+      Helper to create and validate URLs based on an Phoenix web app's routes.
+      """,
+      docs: docs()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
+  defp elixirc_paths(:test), do: ["lib", "test"]
+  defp elixirc_paths(_), do: ["lib"]
+
   def application do
     [
       extra_applications: [:logger],
@@ -19,11 +31,27 @@ defmodule Directions.MixProject do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, "~> 0.27", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      links: %{"GitHub" => @scm_url},
+      licenses: ["MIT"],
+      maintainers: ["Cássio Marques"],
+      files: ~w(lib config mix.exs README.md LICENSE.md)
+    ]
+  end
+
+  defp docs do
+    [
+      main: "Directions",
+      source_ref: "v#{@version}",
+      canonical: "https://hexdocs.pm/directions",
+      source_url: @scm_url
     ]
   end
 end
